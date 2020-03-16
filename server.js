f22f095e647b47151b8eed2299a252e8cdfba200
Eze Sunday Eze <mailsforeze@gmail.com>
2020-03-16 14:02:47 +0100

    fix(removed body-parser): removed body-parser as an external package since it's already part of express 4.*

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const kenx = require("knex");
 
 const db = kenx({
@@ -13,8 +12,8 @@ const db = kenx({
 });
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
