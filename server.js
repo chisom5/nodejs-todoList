807a8daeaa16632cc47f2f3548e21b98712bb501
chisom5 <okoyechisom54@gmail.com>
2020-03-16 12:38:48 +0100

    completed the todo list app

--- server.js
+++ server.js
@@ -1,23 +1,65 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const kenx = require("knex");
+
+const db = kenx({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "chisom05",
+    database: "todo_list"
+  }
+});
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-// create todo task
-app.post('/createTask', (req, res)=> {
-
+// res.render
+app.get("/", (req, res) => {
+  db.select("*")
+    .from("task")
+    .then(data => {
+      res.render("index", { todos: data });
+    })
+    .catch(err => res.status(400).json(err));
 });
 
+// create new task
+app.post("/addTask", (req, res) => {
+  const { textTodo } = req.body;
+  db("task")
+    .insert({ task: textTodo })
+    .returning("*")
+    .then(todo => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.status(400).json({ message: "unable to create a new task" });
+    });
+});
 
-app.listen(8080, ()=> console.log('app is running on port 8080'));
+app.put("/moveTaskDone", (req, res) => {
+  const { name, id } = req.body;
 
+  if (name === "todo") {
+    db("task")
+      .where("id", "=", id)
+      .update("status", 1)
+      .returning("status")
+      .then(task => {res.json(task[0])});
+  } else {
+    db("task")
+      .where("id", "=", id)
+      .update("status", 0)
+      .returning("status")
+      .then(task => {res.json(task[0])});
+  }
+});
 
-/*
+app.listen(8080, () => console.log("app is running on port 8080"));
 
-2 tables
-- todo table
-- done table
-
-*/
